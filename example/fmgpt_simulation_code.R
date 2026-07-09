38cbcf89710f1067b4d6dec62d299ef2e14c7ecc
tacanida <135726976+tacanida@users.noreply.github.com>
2026-07-09 19:13:08 -0400

    Add a reproducible example

---
+++ example/fmgpt_simulation_code.R
@@ -0,0 +1,32 @@
+#This code generates an example data set following the same process for Scenario 1
+#when there are 50 continuous outcome variables and only 1 underlying factor
+
+#generate 100 genes, 10 cis-SNPs per gene, 10 genes per region
+
+dat <- gen_twas_sets(n_qtl = 250, n_gwas = 5000, n_genes = 100, snps_per_gene = 10, block_sizes = rep(10, 100), rho_between = 0.33, rho_within = 0.66, response_types = c(50, 0, 0), r = 0.1, k = 1, maf = 0.24, choice = "genes", sig_rho = 0.66, pi0 = 1, pi1 = 0.1, pi2 = 1, expression_heritability = 0.1,sigma2 = 1, binflate = 0.05)
+while(sum(dat$A) == 0) {
+  dat <- gen_twas_sets(n_qtl = 250, n_gwas = 5000, n_genes = 100, snps_per_gene = 10, block_sizes = rep(10, 100), rho_between = 0.33, rho_within = 0.66, response_types = c(50, 0, 0), r = 0.1, k = 1, maf = 0.24, choice = "genes", sig_rho = 0.66, pi0 = 1, pi1 = 0.1, pi2 = 1, expression_heritability = 0.1,sigma2 = 1, binflate = 0.05)
+}
+
+est_coef_mat <- matrix(nrow = nrow(dat$SNP_coef), ncol = ncol(dat$SNP_coef))
+
+for(j in 1:ncol(dat$SNP_coef)) {
+  tmp_out <- lm.spike(scale(dat$E_qtl[,j], scale = FALSE) ~ scale(dat$G_qtl, scale = FALSE)+0, niter = 3000)
+  est_coef_mat[,j] <- apply(tmp_out$beta[2000:3000,], 2, median)
+}
+
+E_gwas <- dat$G_gwas %*% est_coef_mat
+
+#now split the data into the 10 distinct regions
+
+split_idx <- rep(c(1:10), each = 10)
+
+fit_list <- vector("list", 10)
+
+for(j in 1:10) {
+  fit_list[[j]] <- FMGPT(X = scale(E_gwas[,which(split_idx == j)], scale = FALSE), Y = scale(dat$Yo, scale = FALSE), A = 0*dat$A[which(split_idx == j)], group = seq(1, 10), response_types = dat$responses, epsilon = 0.4, B = 3000, burnin = 1000, thin = 1, k_fix = NULL)
+}
+
+#get all pips
+
+gene_pips <- lapply(fit_list, FUN = function(x) twas_pip(x, max(x$kstar)))
