2225111e275c66133686d38d608640e5323a3277
tacanida <135726976+tacanida@users.noreply.github.com>
2026-07-10 12:23:02 -0400

    Update bfdr.R

--- R/bfdr.R
+++ R/bfdr.R
@@ -1,14 +1,14 @@
 bfdr <- function(pips, cutoff) {
-  phat <- 1 - pips
-  tseq <- seq(min(phat), 1, by=1e-4)
-  bfdrt <- numeric(length(tseq))
-  counter <- 1
-  for(t in tseq) {
-    dk <- as.numeric(phat < t)
-    bfdrt[counter] <- sum(phat * dk) / sum(dk)
-    counter <- counter + 1
-  }
-  diffs <- abs(cutoff - bfdrt)
-  tchoice <- min(which(diffs == min(diffs, na.rm = TRUE)))
-  return(1 - tseq[tchoice])
+    yy <- cumsum(sort(1-pips))/(1:length(pips))
+    mm <- yy < cutoff
+    idx <- sum(mm)
+    if (idx > 0) {
+        out <- 1 - sort(pips)[index]
+    }
+    if (idx == 0) {
+        out <- 1
+        warning("no valid cutoff found")
+    }
+    names(out) <- NULL
+    return(out)
 }
